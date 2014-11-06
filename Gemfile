7c5f1339868cc8de95cc70660288a0618ab192b8
Daniel Cepeda <LanguiDude@gmail.com>
2014-11-06 15:36:05 +0000

    Users can sign up

--- Gemfile
+++ Gemfile
@@ -1,8 +1,9 @@
 source 'https://rubygems.org'
 
 gem 'rails',                '4.2.0.beta4'
+gem 'bootstrap-sass',        '3.2.0.0'
 gem 'bcrypt',               '3.1.7'
-gem 'sass-rails',           '5.0.0.beta1'
+gem 'sprockets-rails',      '>= 2.0'
 gem 'uglifier',             '2.5.3'
 gem 'coffee-rails',         '4.0.1'
 gem 'jquery-rails',         '4.0.0.beta2'
@@ -15,7 +16,7 @@ gem 'twitter-bootstrap-rails', '2.2.8'
 gem 'sdoc',                 '0.4.0', group: :doc
 
 group :test do
-	gem 'cucumber-rails'
+	gem 'cucumber-rails', :require => false
 	gem 'rspec'
 	gem 'database_cleaner'
 	gem 'selenium-webdriver'
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -5,7 +5,7 @@ class UserTest < ActiveSupport::TestCase
   #   assert true
   # end
   def setup
-      @user = User.new(name: "Example User", email: "user@example.com",
+      @user = User.new(email: "user@example.com",
                       password: "foobar", password_confirmation: "foobar")
   end
   
@@ -13,18 +13,8 @@ class UserTest < ActiveSupport::TestCase
     assert @user.valid?
   end
   
-  test "name should be present" do
-    @user.name = "     "
-    assert_not @user.valid?
-  end
-  
   test "email should be present" do
     @user.email = "     "
-    assert_not @user.valid?
-  end
-  
-  test "name should not be too long" do
-    @user.name = "a" * 51
     assert_not @user.valid?
   end
   
@@ -44,7 +34,7 @@ class UserTest < ActiveSupport::TestCase
   
   test "email validation should reject invalid addresses" do
     invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
-                           foo@bar_baz.com foo@bar+baz.com]
+                           foo@bar_baz.com foo@bar+baz.com foo@bar..com]
     invalid_addresses.each do |invalid_address|
       @user.email = invalid_address
       assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
